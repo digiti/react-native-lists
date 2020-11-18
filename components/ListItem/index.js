2f9fce13ab56f7ff2dfd690473377389f2a5c647
Jasper Lammens <lammensjasper@gmail.com>
2020-11-18 13:28:03 +0100

    Change type of ListItem-children

--- components/ListItem/index.js
+++ components/ListItem/index.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 
 class ListItem extends Component {
   static propTypes = {
-    children: PropTypes.array,
+    children: PropTypes.array | PropTypes.element,
     customStyle: PropTypes.object,
   };
 
